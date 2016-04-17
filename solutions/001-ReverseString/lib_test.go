ceee02b5117556476be456bad60135b27f65df23
name <freiny50@gmail.com>
2016-04-16 21:25:08 -0500

    Update test code for example

--- solutions/001-ReverseString/lib_test.go
+++ solutions/001-ReverseString/lib_test.go
@@ -1,12 +1,23 @@
 package lib
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestReverse(t *testing.T) {
-	var got, expect string
-	got = Reverse("asdf")
-	expect = "fdsa"
-	if got != expect {
-		t.Error("Expected ", expect, ", got ", got)
+	m := map[string]string{
+		"asdf":        "fdsa",
+		"vU _!12 ?3P": "P3? 21!_ Uv",
+		"":            "",
 	}
+	for in, out := range m {
+		got := Reverse(in)
+		if got != out {
+			t.Error("fn(", in, ") =>", got, "!=", out)
+		} else {
+			fmt.Println("PASS: fn(", in, ") =>", got, "==", out)
+		}
+	}
+
 }
